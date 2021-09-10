5f20d4331f1d6623128e0f3a232db1e954f0ceb0
bverfurth <78745926+bverfurth@users.noreply.github.com>
2021-09-10 13:07:47 -0400

    wip

--- api/server.js
+++ api/server.js
@@ -1,7 +1,14 @@
 const express = require("express");
-const recipesRouter = require("./api/recipes-router");
-server = express();
+const recipesRouter = require("./recipes/recipes-router");
+
+const server = express();
 
 server.use(express.json());
 
 server.use("/api/recipes", recipesRouter);
+
+server.use("*", (req, res) => {
+  res.json({ api: "up" });
+});
+
+module.exports = server;
